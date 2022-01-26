0bebf59d7621c0e27d232f77ec1e8198a2e0e893
turtle601 <turtle601@naver.com>
2022-01-26 13:55:49 +0900

    feat : resetCSS를 통한 global style setting #4

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { globalStyle } from './styles/global';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
