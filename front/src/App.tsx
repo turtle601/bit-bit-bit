6564eb74f192018e9e9608adecd124b7a9a44778
turtle601 <turtle601@naver.com>
2022-01-26 13:11:45 +0900

    chores: eslint(airbnb) + prettier 설정 #2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
-  return (
-    <>
-      <div>hi</div>
-    </>
-  );
+const App: React.FC = () => {
+  return <h1>Hello, world!</h1>;
 };
 
 export default App;
