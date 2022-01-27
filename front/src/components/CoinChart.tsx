c8dfc6d58f68873a51e261cda45fca25df74a399
turtle601 <turtle601@naver.com>
2022-01-27 22:24:11 +0900

    feat: chart 1차 구현 #23

--- front/src/components/CoinChart.tsx
+++ front/src/components/CoinChart.tsx
@@ -1,19 +1,74 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
 
 import { fetchCoinChartApi } from '../api/api';
 
-interface CoinIdProps {
-  coinId: string | undefined;
+import { CoinIdProps } from '../types/types';
+
+interface chartDateType {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
 }
 
 const CoinChart = ({ coinId }: CoinIdProps) => {
+  const [chartdataList, setChartItem] = useState<chartDateType[]>();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     (async () => {
       const chartData = await fetchCoinChartApi(coinId);
-      console.log(chartData);
+      setChartItem(chartData);
+      setLoading(false);
     })();
   }, [coinId]);
-  return <>CoinChart</>;
+  return (
+    <div>
+      {loading ? (
+        'Loading chart...'
+      ) : (
+        <Chart
+          type="line"
+          series={[
+            {
+              name: 'Price',
+              data: chartdataList?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: 'transparent',
+            },
+            grid: { show: false },
+            stroke: {
+              curve: 'smooth',
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default CoinChart;
