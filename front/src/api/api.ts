a6c54c77663067116ad041fac914a49f710ca4ac
turtle601 <turtle601@naver.com>
2022-01-27 15:26:21 +0900

    feat : api /coinId /ticket/coinId fetch #17

--- front/src/api/api.ts
+++ front/src/api/api.ts
@@ -3,3 +3,19 @@ export const fetchCoinsApi = async () => {
   const fetchCoinData = await response.json();
   return fetchCoinData;
 };
+
+export const fetchCoinIdApi = async <T>(coinId: T) => {
+  const response = await fetch(
+    `https://api.coinpaprika.com/v1/coins/${coinId}`,
+  );
+  const fetchCoinId = await response.json();
+  return fetchCoinId;
+};
+
+export const fetchCoinPriceApi = async <T>(coinId: T) => {
+  const response = await fetch(
+    `https://api.coinpaprika.com/v1/tickers/${coinId}`,
+  );
+  const fetchCoinPrice = await response.json();
+  return fetchCoinPrice;
+};
--- front/src/pages/coinDetail.tsx
+++ front/src/pages/coinDetail.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ThemeProps } from '../types/types';
+
+import { fetchCoinIdApi, fetchCoinPriceApi } from '../api/api';
+
 import { Container } from '../layout/layout';
 
 import Title from '../components/Title';
 
 const CoinDetail = ({ theme, toggleTheme }: ThemeProps) => {
-  const { coinId } = useParams<string>();
+  const { coinId } = useParams();
+
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const apiCoinId = await fetchCoinIdApi(coinId);
+      const apiCoinPrice = await fetchCoinPriceApi(coinId);
+
+      setInfo(apiCoinId);
+      setPrice(apiCoinPrice);
+    })();
+  }, [coinId]);
 
   return (
     <Container>
       <Title theme={theme} toggleTheme={toggleTheme} name={coinId} />
+      <div>{JSON.stringify(info)}</div>
+      <div>{JSON.stringify(price)}</div>
     </Container>
   );
 };
